079df33654403748ef665373edecc3fbabb42bba
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-09-22 15:29:12 -0400

    Created the reply app + created the serializers for both apps

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -7,7 +7,7 @@ class Comment(models.Model):
 
     user =models.ForeignKey(User, on_delete=models.CASCADE)
     video_id = models.CharField(max_length=150)
-    text = models.CharField(max_length=150)
+    text = models.CharField(max_length=500)
     likes = models.IntegerField()
     dislikes = models.IntegerField()
 
