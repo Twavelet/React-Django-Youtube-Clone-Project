d0d03f1db2b6a0faabf26a822cc8f648325a3116
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-09-22 16:05:12 -0400

    Updated the views.py file in the comment app to include the functionality for GET, POST, GET (by id), and PUT requests

--- backend/comment/views.py
+++ backend/comment/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+from backend import comment
+from .serializers import CommentSerializer
+from .models import Comment
+from comment import serializers
 # Create your views here.
+
+
+@api_view(['GET', 'POST'])
+def comments_list(request):
+    if request.method == 'GET':
+
+        comment = Comment.objects.all()
+        serializer = CommentSerializer(comment, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT'])
+def comments_detail(request, pk):
+    
+    comment = get_object_or_404(Comment, pk=pk)
+    if request.method == 'GET':
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CommentSerializer(comment, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
