7787460053b0835f067f15a3cff53322946c81e0
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-09-22 17:28:51 -0400

    Made migrations to create the comment and reply tables in mysql

--- backend/comment/views.py
+++ backend/comment/views.py
@@ -3,23 +3,16 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
-from backend import comment
 from .serializers import CommentSerializer
 from .models import Comment
 from comment import serializers
 # Create your views here.
 
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 def comments_list(request):
-    if request.method == 'GET':
 
-        comment = Comment.objects.all()
-        serializer = CommentSerializer(comment, many = True)
-        return Response(serializer.data, status = status.HTTP_200_OK)
-
-    elif request.method == 'POST':
+    if request.method == 'POST':
         serializer = CommentSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
