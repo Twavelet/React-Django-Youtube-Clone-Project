f2086ebeaba8d9ce8647cb03bbd14d543f0c6bac
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-09-22 16:17:02 -0400

    Updated the reply app's views.py to include the GET and POST request functionality + Updated the endpoints in the urls.py file

--- backend/Reply/views.py
+++ backend/Reply/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ReplySerializer
+from .models import Reply
+from reply import serializers
 # Create your views here.
+
+@api_view(['POST'])
+def reply_list(request):
+    if request.method == 'POST':
+        serializer = ReplySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def reply_detail(request, pk):
+
+    reply= get_object_or_404(Reply, pk=pk)
+    if request.method == 'GET':
+        serializer = ReplySerializer(reply)
+        return Response(serializer.data)
