54fb9576223ce8eaede96a136e2093f4852acd16
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-11-01 15:31:54 -0400

    Updated the Videopage.jsx file to take out the map function. It was not needed as the video_id for the chosen video is already being passed in. The map function was causing it to produce multiple iframes instead of a singular one

--- frontend/src/components/VideoPage/VideoPage.jsx
+++ frontend/src/components/VideoPage/VideoPage.jsx
@@ -1,34 +1,24 @@
 import { useParams } from "react-router-dom";
 
-
 const VideoPage = (props) => {
+  const { videoId } = useParams();
 
-    const { videoId } = useParams()
+  //   title={element.snippet.title}
+  //   description={element.snippet.description}
 
-
-    return(
-    
-        <div>
-        {props.parentData.map((element) => {
-
-            return(
-                <div>
-                    <h1>Testing to see if the routing works {videoId} </h1>
-                    <iframe id="ytplayer" type="text/html" width="640" height="360"
-                        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&origin=http://example.com`}
-                        frameborder="0"
-                        title={element.snippet.title}
-                        description={element.snippet.description}>
-                    </iframe>
-                </div>
-            )
-            
-        
-
-        
-    })}
+  return (
+    <div>
+      <h1>Testing to see if the routing works {videoId} </h1>
+      <iframe
+        id="ytplayer"
+        type="text/html"
+        width="640"
+        height="360"
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&origin=http://example.com`}
+        frameborder="0"
+      ></iframe>
     </div>
-    )
-}
+  );
+};
 
 export default VideoPage;
